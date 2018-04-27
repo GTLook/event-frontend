f8fcc8759c24320aa75bb9deba68d61c36c3fb35
Gavin Look <GTLook@gmail.com>
2018-04-26 21:21:24 -0700

    fixing data table

--- src/analytics.js
+++ src/analytics.js
@@ -3,41 +3,37 @@ request('/auth/token')
 .then((response) => { })
 .catch((error) => { window.location = '/index.html' })
 
+const colors = ['rgb(229, 54, 39)',
+                'rgb(255, 161, 0)',
+                'rgb(255, 242, 0)',
+                'rgb(70, 219, 32)',
+                'rgb(21, 68, 239)',
+                'rgb(209, 20, 239)']
+
 const chart1 = document.getElementById('chart1').getContext('2d');
 const chart2 = document.getElementById('chart2').getContext('2d');
 const chart3 = document.getElementById('chart3').getContext('2d');
 const chart4 = document.getElementById('chart4').getContext('2d');
+const chart5 = document.getElementById('chart4').getContext('2d');
 
+request('/data')
+.then((response) => {
+  console.log(response.data)
+})
+
+//line chart
 const objChart1 = new Chart(chart1, {
-  // The type of chart we want to create
   type: 'line',
-  // The data for our dataset
   data: {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [{
-      label: "My First dataset",
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rctxgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
+      label: "Months Working",
+      backgroundColor: colors,
+      borderColor: colors,
       data: [0, 10, 5, 2, 20, 30, 45],
     }]
-  },
-  // Configuration options go here
-  options: {}
-});
+  }
+})
 
 
 const objChart2 = new Chart(chart2, {
@@ -47,22 +43,8 @@ const objChart2 = new Chart(chart2, {
     datasets: [{
       label: '# of Votes',
       data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rctxgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
+      backgroundColor: colors,
+      borderColor: colors,
       borderWidth: 1
     }]
   },
@@ -85,23 +67,9 @@ const objChart3 = new Chart(chart3, {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [{
       label: "My First dataset",
-      data: [0, 10, 5, 2, 20, 30, 45],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rctxgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
+      backgroundColor: colors,
+      borderColor: colors,
+      data: [0, 10, 5, 2, 20, 30, 45]
     }]
   },
   // Configuration options go here
@@ -116,23 +84,9 @@ const objChart4 = new Chart(chart4, {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [{
       label: "My First dataset",
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rctxgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      data: [0, 10, 5, 2, 20, 30, 45],
+      backgroundColor: colors,
+      borderColor: colors,
+      data: [0, 10, 5, 2, 20, 30, 45]
     }]
   },
   // Configuration options go here
--- src/data.js
+++ src/data.js
@@ -3,58 +3,120 @@ request('/auth/token')
 .then((response) => { })
 .catch((error) => { window.location = '/index.html' })
 
-//jQuery selectors for editable table
-const $TABLE = $('#table');
-const $BTN = $('#export-btn');
-const $EXPORT = $('#export');
+//request data from data db and render table
+request('/data')
+.then((response) => {
+  Object.keys(response.data.data[0]).forEach(key => {
+    //table headers
+    const tableHead = document.querySelector('#tableHead')
+    const newHead = document.createElement("th")
+    newHead.setAttribute('scope','col')
+    newHead.innerHTML = key
+    tableHead.appendChild(newHead)
+  })
+  return response
+})
+.then((response) => {
+  const tableBody = document.querySelector('#tableBody')
+  response.data.data.forEach(obj => {
+    const newRow = document.createElement('tr')
+    //newRow.setAttribute('scope','row')
+    for(let element in obj){
+      if(element === 'Main'){
+        const newCell = createMainDrop(obj)
+        newRow.appendChild(newCell)
+      }
+      else if(element === 'Secondary'){
+        const newCell = createSecondDrop(obj)
+        newRow.appendChild(newCell)
+      }
+      else{
+        const newCell = document.createElement('td')
+        newCell.setAttribute('data-id', obj.id)
+        newCell.setAttribute('data-element', element)
+        newCell.addEventListener('blur',function(event){
+          // console.log(event.target.innerHTML)
+          // console.log(event.target.getAttribute('data-id'))
+          // console.log(event.target.getAttribute('data-element'))
+          request(`/data/${event.target.getAttribute('data-id')}`, 'put', {element:event.target.getAttribute('data-element'), data: event.target.innerHTML})
+          .then(response => {
+            event.target.innerHTML = resonse.data
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+        })
 
-$('.table-add').click(() => {
-  const $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
-  $TABLE.find('table').append($clone);
-});
+        if(element == 'id') newCell.setAttribute('scope','row')
+        else newCell.setAttribute('contenteditable',true)
+        if(element === 'Start' || element === 'End') newCell.innerHTML = moment(obj[element]).format('YYYY-MM-DD HH:MM')
+        else newCell.innerHTML = obj[element]
+        newRow.appendChild(newCell)
+      }
+    }
+    tableBody.appendChild(newRow)
+  })
+  return response
+})
+.then((response)=>{  //creates empty row on bottom
+  const tableBody = document.querySelector('#tableBody')
+  const newRow = document.createElement('tr')
+  let idCount = 0
+  Object.keys(response.data.data[0]).forEach(key => {
+      idCount++
+      const newCell = document.createElement("td")
+      if(idCount==1) newCell.innerHTML = response.data.data.length + 1
+      else newCell.setAttribute('contenteditable',true)
+      newRow.appendChild(newCell)
+    })
+    tableBody.appendChild(newRow)
+})
 
-$('.table-remove').click(() => {
-  $(this).parents('tr').detach();
-});
 
-$('.table-up').click(() => {
-  const $row = $(this).parents('tr');
-  if ($row.index() === 1) return; // Don't go above the header
-  $row.prev().before($row.get(0));
-});
+const createMainDrop = (obj) => {
+  const mainDropValues = [{name:'Start Event', id: 1}, {name:'Pause Event', id:2}, {name:'End Event',id:3}]
+  //const newDropdown = document.createElement('button')
+  obj.name = obj.Main
+  return dropdown(obj, mainDropValues)
+}
 
-$('.table-down').click(function () {
-  const $row = $(this).parents('tr');
-  $row.next().after($row.get(0));
-});
+const createSecondDrop = (obj) => {
+  const mainDropValues = [{name:'Starting up', id:1}, {name:'Running', id:2}, {name:'Slowing Down', id:3},{ name:'Maintenance', id:4},
+                          {name:'Starting back up', id:5}, {name:'failed', id:6}, {name:'Success', id:7}, {name:'Waiting on materials', id:8}, {name:'Waiting on customer', id:9}]
+  //const newDropdown = document.createElement('button')
+  // console.log(obj)
+  obj.name = obj.Secondary
+  return dropdown(obj, mainDropValues)
+}
 
-// A few jQuery helpers for exporting only
-jQuery.fn.pop = [].pop;
-jQuery.fn.shift = [].shift;
+const dropdown = (obj, dropValues) => {
+  const td = document.createElement('td')
+  const label = document.createElement('button')
+  label.classList.add('btn')
+  // label.classList.add('btn-secondary')
+  label.classList.add('dropdown-toggle')
+  label.id = 'dropdownMenuLink'
+  label.setAttribute('data-toggle', 'dropdown')
+  // label.setAttribute('aria-haspopup', 'true')
+  // label.setAttribute('aria-expanded', 'false')
+  label.innerHTML = obj.name
+  td.appendChild(label)
 
-$BTN.click(function () {
-  const $rows = $TABLE.find('tr:not(:hidden)');
-  const headers = [];
-  const data = [];
+  const dropdown = dropValues.reduce((acc, ele)=> {
+    const a = document.createElement('a')
+    a.classList.add('dropdown-item')
+    a.href = '#'
+    a.setAttribute('data-id', ele.id)
+    a.innerHTML = ele.name
+    acc.appendChild(a)
+    a.addEventListener('click', function(event){
+      label.innerHTML = event.target.innerHTML
 
-  // Get the headers (add special header logic here)
-  $($rows.shift()).find('th:not(:empty)').each(function () {
-    headers.push($(this).text().toLowerCase());
-  });
+    })
+    return acc
+  }, document.createElement('div'))
+  dropdown.classList.add('dropdown-menu')
+  td.appendChild(dropdown)
 
-  // Turn all existing rows into a loopable array
-  $rows.each(function () {
-    const $td = $(this).find('td');
-    const h = {};
-
-    // Use the headers from earlier to name our hash keys
-    headers.forEach(function (header, i) {
-      h[header] = $td.eq(i).text();
-    });
-
-    data.push(h);
-  });
-
-  // Output the result
-  $EXPORT.text(JSON.stringify(data));
-});
+  return td
+}
